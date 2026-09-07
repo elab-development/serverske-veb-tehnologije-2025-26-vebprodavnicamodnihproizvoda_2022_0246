5df05691c5f1dad8c238f6c327cf5a912bf019b6
Saska Savic <saskasavic1809@gmail.com>
2026-09-07 19:24:32 +0200

    Dodate stavke porudzbine i veze izmedju modela

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory; 
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'brand',
+        'description',
+        'image',
+        'price',
+        'stock',
+        'size',
+    ];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
     }
 }
