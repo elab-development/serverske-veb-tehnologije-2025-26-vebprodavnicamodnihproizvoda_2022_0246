43694415ce92dd16615f8c30242b77abac6c310d
Jana Simic <janasimic03@gmail.com>
2026-09-08 20:08:32 +0200

    Implementiran REST API za porudzbine

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -15,6 +16,8 @@ Route::apiResource('products', ProductController::class)
     ->only(['index', 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::apiResource('orders', OrderController::class);
 
     Route::post('logout', [AuthController::class, 'logout']);
 
